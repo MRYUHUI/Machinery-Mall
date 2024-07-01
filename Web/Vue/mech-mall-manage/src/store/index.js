3560729fb3619f5be1357d6bea5cdd39a03f278c
hang <2521153494@qq.com>
2024-07-01 09:56:34 +0800

    恢复index

---
+++ Web/Vue/mech-mall-manage/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore } from "vuex";
+import createPersistedState from 'vuex-persistedstate';
+import user from "./user";
+import admin from "./admin";
+import product from "./product";
+
+const store = createStore({
+	modules: {
+		user,
+		admin,
+		product,
+	},
+	plugins: [
+		createPersistedState({
+			key: 'my-app-state', // 自定义存储键名
+		}),
+	],
+});
+
+export default store;
+
+// // 在页面即将刷新时，存储本地存储
+// window.addEventListener('beforeunload', () => {
+// 	const currentState = store.state; // 获取当前 Vuex 状态
+// 	localStorage.setItem('my-app-state', JSON.stringify(currentState)); // 存储到本地存储
+// });
